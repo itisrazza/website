ad33880c518bb9b53cb3b198b50cdaa63e04a09f
Raresh Nistor <raresh@nistor.email>
2022-02-21 22:51:54 +1300

    move to razza.io & font change

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -41,7 +41,7 @@ export default function About() {
             <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
               React
             </a>{" "}
-            and (will soon be) hosted on{" "}
+            and is hosted on{" "}
             <a
               href="https://pages.github.com/"
               target="_blank"
@@ -59,6 +59,16 @@ export default function About() {
             </a>
             .
           </p>
+          <p>Typography wise, I'm using{" "}
+            <a href="https://rsms.me/inter/"
+            target="_blank"
+            rel="noreferrer">Inter</a>{" "}
+            for text <a href="https://rsms.me/inter/lab/?feat-cv05=1&feat-cv11=1&feat-ss01=1&feat-ss03=1&varSlant=4.01&varfont=1" 
+            target="_blank"
+            rel="noreferrer">with a few tweaks</a> and <a href="https://www.jetbrains.com/lp/mono/"
+              target="_blank"
+              rel="noreferrer"><code>Jetbrains Mono</code>
+              </a> for the code bits.</p>
         </div>
       </main>
     </>
